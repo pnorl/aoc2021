c550c04c742febc22c71b1e3ff825284974e3389
Patrik Norlin <patrik.norlin@frisq.se>
2022-01-01 20:15:54 +0100

    update template

--- template.py
+++ template.py
@@ -1,10 +1,17 @@
-lines = open("0.in").read().splitlines()
+def part1(lines):
+    return "not solved"
 
-part1 = 0
-part2 = 0
+def part2(lines):
+    return "not solved"
 
-for line in lines:
-    pass
+def format_input(filename):
+    lines = open(filename).read().splitlines()
+    return lines
 
-print("part1", part1)
-print("part2", part2)
+def main():
+    lines = format_input("0.in")
+    print("part1", part1(lines))
+    print("part2", part2(lines))
+
+if __name__ == '__main__':
+    main()
